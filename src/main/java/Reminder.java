376c5a6c3632efb2be00fafd7170a45434983869
dianmink <diana.minko@tele2.com>
2017-05-16 16:33:40 +0300

    saying goodbye

--- src/main/java/Reminder.java
+++ src/main/java/Reminder.java
@@ -1,16 +1,29 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Created by dianmink on 11-05-2017.
  */
 public class Reminder extends Note {
 
-    private String DateTime;
+    private LocalDateTime DateTime;
 
-    public String getDateTime() {
+    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+
+
+    public LocalDateTime getDateTime() {
         return DateTime;
     }
 
-    public void setDateTime(String dateTime) {
+    public void setDateTime(LocalDateTime dateTime) {
         DateTime = dateTime;
+    }
+
+    public void setDateTime(String strTime) {
+//convert string to datetime
+//       DateTime=LocalDateTime.parse(strTime);
+        DateTime = LocalDateTime.parse(strTime, fmt);
+
     }
 
     @Override
